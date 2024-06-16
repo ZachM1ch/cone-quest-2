2def5b49dd738a459c03e2155b1984b8433462a9
Ryan Torres <rytorres99@gmail.com>
2024-06-15 23:51:36 -0500

    added comments and fixed added mesh rotation behavior back

--- ConeQuest2/Assets/Scripts/StatTracker.cs
+++ ConeQuest2/Assets/Scripts/StatTracker.cs
@@ -10,13 +10,6 @@ public class StatTracker : MonoBehaviour
     public Rigidbody playerRb;
     public TMP_Text text;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         text.SetText("Speed: " + playerRb.velocity.magnitude.ToString());
