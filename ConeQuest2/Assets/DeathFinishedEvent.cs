544f72dbcee9d5fb31a08751c2b637ccb2540801
Ryan Torres <rytorres99@gmail.com>
2024-06-19 05:57:55 -0500

    enemies can now be killed and be setup really easily

--- ConeQuest2/Assets/DeathFinishedEvent.cs
+++ ConeQuest2/Assets/DeathFinishedEvent.cs
@@ -19,7 +19,7 @@ public class DeathFinishedEvent : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Destroy(animator.gameObject.transform.parent);
+        Destroy(animator.gameObject.transform.parent.gameObject);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
